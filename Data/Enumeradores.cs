386f6b4c7ba175e5227367bff4f19e558d4c63a7
dpedrazas <dpedrazas@P0103FPWS21>
2017-08-18 21:29:14 -0400

    Capas de Datos

---
+++ Data/Enumeradores.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data
+{
+    class Enumeradores
+    {
+        public enum tipoAccion : byte
+        {
+            NoAction = 0,
+        Insert = 1,
+        Update = 2,
+        Delete = 3
+        } 
+       
+    
+    }
+}

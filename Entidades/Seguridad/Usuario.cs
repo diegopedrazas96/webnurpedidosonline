dcf87f882ddad6d36e6085ea8f869ef6abfeaf6f
dpedrazas <dpedrazas@P0103FPWS21>
2017-09-08 19:53:31 -0400

    Estructura Proyecto

---
+++ Entidades/Seguridad/Usuario.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades.Seguridad
+{
+    public class Usuario : Entidades.Entidad
+    {
+        public long Id { get; set; }
+        public long TipoIdc { get; set; }
+        public long? CompaniaId { get; set; }
+        public string Codigo { get; set; }
+        public string Nombre { get; set; }
+        public string Email { get; set; }
+        public string Login { get; set; }      
+        public string Password { get; set; }
+        public string Telefono { get; set; }
+        public bool Estado { get; set; }
+        public Usuario()
+        {
+
+        }
+
+        
+
+    } 
+
+    
+    public enum relUsuario
+    {
+        Compania
+        
+    }
+}
+
--- WebNurPedidosOnline/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WebNurPedidosOnline.MvcApplication" Language="C#" %>

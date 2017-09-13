57a69a0c32f8702a2ce3ecd7a9fa4e42782f3a2a
dpedrazas <dpedrazas@P0103FPWS21>
2017-09-12 21:27:00 -0400

    Cambios Aplicacion 1.0.1

---
+++ WebNurServiciosPedidos/Controllers/UsuarioController.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Security.Claims;
+using System.Security.Cryptography;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Http;
+using System.Web.Http.ModelBinding;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.AspNet.Identity.Owin;
+using Microsoft.Owin.Security;
+using Microsoft.Owin.Security.Cookies;
+using Microsoft.Owin.Security.OAuth;
+using WebNurServiciosPedidos.Models;
+using WebNurServiciosPedidos.Providers;
+using WebNurServiciosPedidos.Results;
+using ES = Entidades.Seguridad;
+
+namespace WebNurServiciosPedidos.Controllers
+{
+    [Authorize]
+    [RoutePrefix("api/Account")]
+    public class UsuarioController : ApiController
+    {
+        // GET api/values
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/values/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        public void Post([FromBody]ES.Usuario objUsuario)
+        {
+            try
+            {
+
+            }
+            catch (Exception e){
+
+            }
+        }
+
+        // PUT api/values/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/values/5
+        public void Delete(int id)
+        {
+        }
+    }
+}

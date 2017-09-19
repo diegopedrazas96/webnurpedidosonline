e6a5f4d63c7faafa6e20531d2fe7faa5edefd10c
jose75684847 <jose75684847@gmail.com>
2017-09-19 17:02:52 -0400

    Change in Data

--- EasyBuyWEB/AdminUsers/RegistUsers.aspx.cs
+++ EasyBuyWEB/AdminUsers/RegistUsers.aspx.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Entidades.Seguridad;
+using Negocio.Seguridad;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -14,6 +16,8 @@ public partial class AdminUsers_RegistUsers : System.Web.UI.Page
 
     protected void btnRegistrar_Click(object sender, EventArgs e)
     {
+        UserBRL userbrl = new UserBRL();
+        User usuario = userbrl.getUserByEmail("jose cadima");
         //Registre Aquí
     }
 }

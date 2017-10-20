7c2989c37f76ceb814e95c6312b8eccf548ee50b
jose75684847 <jose75684847@gmail.com>
2017-10-20 18:31:30 -0400

    Base Versionada Stanley

--- WebEjtor/Db/V-1-5-0/Update V-1-5-0.sql
+++ WebEjtor/Db/V-1-5-0/Update V-1-5-0.sql
@@ -1,3 +1,56 @@
+Use [Master]
+GO 
+
+IF  NOT EXISTS (SELECT name FROM sys.databases WHERE name = N'bdTienda')
+	RAISERROR('La base de datos ''bdTienda'' no existe. Cree la base de datos primero',16,127)
+GO
+
+USE [bdTienda]
+GO
+
+PRINT 'Actualizando a la version 1.5.0'
+
+IF NOT EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[usp_VERSION_GetVersion]') AND type in (N'P', N'PC'))
+BEGIN
+
+	RAISERROR('La base de datos no esta inicializada',16,127)
+	RETURN;
+
+END
+
+DECLARE @intVersionMayor int
+DECLARE @intVersionMenor int
+DECLARE @intPatch int
+
+EXECUTE [dbo].[usp_VERSION_GetVersion] 
+   @intVersionMayor OUTPUT
+  ,@intVersionMenor OUTPUT
+  ,@intPatch OUTPUT
+
+IF @intVersionMayor IS NULL OR @intVersionMenor IS NULL
+BEGIN
+	
+	RAISERROR('La base de datos no esta inicializada',16,127)
+	RETURN;
+
+END
+
+
+IF @intVersionMayor IS NULL OR @intVersionMenor IS NULL OR NOT (@intVersionMayor = 1 AND @intVersionMenor = 4)
+BEGIN
+	
+	RAISERROR('La base de datos no esta en la version 1.4. Este script solamente se apllica a la version 1.4',16,127)
+	RETURN;
+
+END
+ELSE
+BEGIN
+	
+	PRINT 'Version OK'
+
+END
+
+
 USE [bdTienda]
 GO
 /****** Object:  Table [dbo].[tblEmpresa]    Script Date: 16/10/2017 21:26:20 ******/
@@ -478,4 +531,18 @@ BEGIN
 	WHERE [tipoMaestroId] = @intTipoMaestroId
 	
 END
+GO
+
+
+DELETE FROM [dbo].[tbl_Version]
+GO
+
+INSERT INTO [dbo].[tbl_Version]
+           ([versionMayor]
+           ,[versionMenor]
+           ,[patch])
+     VALUES
+           (1
+           ,5
+           ,0)
 GO
